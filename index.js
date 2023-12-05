4e06cd763faf5fc1344ae7ba0b07837b74271d75
Victor M. Rodriguez <victordoblezz@gmail.com>
2023-12-04 23:22:46 -0400

    docs: cors all *

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 require('dotenv').config();
 
 // Selecciona el archivo .env correspondiente según el entorno
