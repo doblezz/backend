9a5856b03b71adb6cc274abfe18e9568ae18a2a2
Victor M. Rodriguez <victordoblezz@gmail.com>
2023-12-04 23:31:56 -0400

    docs: cors http://localhost:5173

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:5173',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,  // Esto permite el envío de cookies
+  credentials: true,
 }));
 
 // Realiza la consulta a la base de datos
