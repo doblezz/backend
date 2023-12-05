8710dbd268cf3b8b31c39a5313305f58d5ee8fb5
Victor M. Rodriguez <victordoblezz@gmail.com>
2023-12-04 23:47:00 -0400

    docs: cors - dome

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://quedaza.netlify.app', 'https://quezada.do/'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
